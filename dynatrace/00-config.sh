1ef20a7f951e9cff14683057759e662c34d40221
Kamil Skorupa <kamil.skorupa@dynatrace.com>
2026-03-13 15:58:48 +0100

    PR2 fix

--- dynatrace/00-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "shared_preload_libraries = 'pg_stat_statements'" >> "/var/lib/postgresql/data/postgresql.conf"
-echo "track_activity_query_size = 4096" >> "/var/lib/postgresql/data/postgresql.conf"
