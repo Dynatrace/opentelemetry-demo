6553c86e43a76413dc6c980fffc526098c83b1b2
Jan Szczepkowski <jan.szczepkowski@dynatrace.com>
2026-05-13 15:31:39 +0000

    code review

--- .github/actions/scripts/set-version.sh
+++ .github/actions/scripts/set-version.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-replace_all() {
-    FROM=$1
-    TO=$2
-    echo "Replacing [$FROM] with [$TO]"
-    grep -rl $FROM . --exclude-dir={.git,node_modules,bin,obj,build} | xargs sed -i "s/$FROM/$TO/g"
-}
-
 BUILD_VERSION=$1
 WORKDIR=${2:-.}
 BUILD_DATE=${3:-$(date '+%Y-%m-%d %H:%M')}
@@ -21,7 +14,4 @@ cd $WORKDIR
 
 echo "Build version: $BUILD_VERSION, date: $BUILD_DATE, commit: $BUILD_COMMIT, path: $(pwd)"
 
-replace_all "1.0.0-astroshop" "$BUILD_VERSION"
-replace_all "{{BUILD_VERSION}}" "$BUILD_VERSION"
-replace_all "{{BUILD_DATE}}" "$BUILD_DATE"
-replace_all "{{BUILD_COMMIT}}" "$BUILD_COMMIT"
+yq -i ".default.image.tag = \"$BUILD_VERSION\"" charts/astroshop/values.yaml
